65f5f4eebd95d5d4c1f2a49cc957edda4af79e3e
Alexandr Satskov <s.satskov@gmail.com>
2021-08-07 12:37:49 +0300

    add 404 response for route user_feedbacks [get]

--- api/user_feedbacks.py
+++ api/user_feedbacks.py
@@ -1,30 +1,43 @@
-from aiohttp import web
+from typing import Union
 from http import HTTPStatus
-from aiohttp_pydantic.oas.typing import r200, r201
+
+from aiohttp import web
+from aiohttp_pydantic.oas.typing import r200, r201, r404
 
 from api.base_view import BaseView
-
 from api.schema import (
-    GetUserFeedbacksResponse, Feedback
+    GetUserFeedbacksResponse, Feedback,
+    AddUserFeedbackResponse,
+    CommonError,
 )
 
 
 class UserFeedbacks(BaseView):
-    async def get(self, user_email: str) -> r200[GetUserFeedbacksResponse]:
+    async def get(
+            self, user_email: str
+    ) -> Union[r201[AddUserFeedbackResponse], r404[CommonError]]:
+
         feedbacks = await self.dao.get_user_feedbacks(user_email=user_email)
+
+        if not feedbacks:
+            return web.json_response(
+                CommonError(message='no one feedback for current user').dict(),
+                status=HTTPStatus.NOT_FOUND
+            )
+
         return web.json_response(
             GetUserFeedbacksResponse(
                 reviews=[
                     Feedback(
-                        user_email=r.user_email,
-                        place_uid=r.place_uid,
-                        rate=r.rate,
-                        feedback_text=r.feedback_text) for r in feedbacks
+                        user_email=f.user_email,
+                        place_uid=f.place_uid,
+                        rate=f.rate,
+                        feedback_text=f.feedback_text) for f in feedbacks
                 ],
             ).dict(),
             status=HTTPStatus.OK)
 
-    async def post(self, feedback: Feedback) -> r201:
+    async def post(self, feedback: Feedback) -> r201[AddUserFeedbackResponse]:
         feedback_id = await self.dao.post_user_feedback(feedback=feedback)
         return web.json_response(
             {"feedback_id": feedback_id},
