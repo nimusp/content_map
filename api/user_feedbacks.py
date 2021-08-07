174aefb4411a25379a163b9123558e1794be024f
Alexandr Satskov <s.satskov@gmail.com>
2021-08-07 13:41:30 +0300

    fix

--- api/user_feedbacks.py
+++ api/user_feedbacks.py
@@ -39,7 +39,7 @@ class UserFeedbacks(BaseView):
 
     async def post(self, feedback: Feedback) -> r201[AddUserFeedbackResponse]:
         feedback_id = await self.dao.post_user_feedback(feedback=feedback)
-        
+
         return web.json_response(
             AddUserFeedbackResponse(
                 feedback_id=feedback_id
